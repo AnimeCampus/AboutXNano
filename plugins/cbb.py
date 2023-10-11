f3eee4e4c8ab277d69478dc0eb80afcf19698343
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-12 01:23:54 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -3,12 +3,12 @@
 # t.me/SharingUserbot & t.me/Lunatic0de
 
 from bot import Bot
-from config import OWNER
+from config import OWNER, ADMINS
 from Data import Data
 from pyrogram import filters
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, Message
-
+from pyrogram import Client, filters
 
 @Bot.on_message(filters.private & filters.incoming & filters.command("about"))
 async def _about(client: Bot, msg: Message):
@@ -57,3 +57,24 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.message.reply_to_message.delete()
         except BaseException:
             pass
+
+@bot.on_message(filters.command("sudolist") & filters.user(ADMINS))
+async def su_list_command(client: Client, message: Message):
+    users_list = ""
+    for user_id in ADMINS:
+        try:
+            user = await client.get_users(user_id)
+            users_list += f"{user.first_name} (@{user.username})\n"
+        except Exception as e:
+            # Handle any errors here
+            users_list += f"User ID {user_id} - Error: {str(e)}\n"
+    
+    # Save the list to an admin.txt file
+    with open("admin.txt", "w") as admin_file:
+        admin_file.write(users_list)
+    
+    await message.reply(f"Admin Users:\n{users_list}")
+    await client.send_document(chat_id=message.chat.id, document="admin.txt")
+
+
+                     
