913d486fe08604af8e10894d5cc9b23fa37991f2
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-14 22:56:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -42,11 +42,13 @@ async def handle_send_command(client, message):
 
         # Check if the command has a message following it
         if len(message.command) > 1:
-            # Check if the message contains media (photos or stickers)
-            if message.reply_to_message and message.reply_to_message.media:
-                # Forward the media to the database channel
-                await message.reply_to_message.forward(CHANNEL_ID)
-                await message.reply("Your media has been sent to the database channel.")
+            # Check if the message contains media (photos)
+            if message.reply_to_message and message.reply_to_message.photo:
+                # Get the photo file ID
+                photo_file_id = message.reply_to_message.photo[-1].file_id
+                # Send the photo to the database channel
+                await client.send_photo(CHANNEL_ID, photo=photo_file_id, caption=f"@GenXNano You Have Photo From\n\n{username}\n(ID: {user.id})")
+                await message.reply("Your photo has been sent to the database channel.")
             else:
                 text = " ".join(message.command[1])
                 # Send the extracted text to the database channel
@@ -56,6 +58,7 @@ async def handle_send_command(client, message):
             await message.reply("Please provide a message to send with the /send command.")
     except Exception as e:
         LOGGER(__name__).warning(e)
+
 
 
         
